f2de236e1762696bf6356e7b3ef49a65a62f406b
Connor Shea <connor.james.shea@gmail.com>
2022-05-27 13:08:59 -0600

    Got working checkboxes rendered off of the series JSON.
    Now to make it prettier and then add actual integration with vglist...

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import VarietyList from './components/VarietyList.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <VarietyList />
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/VarietyList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+import { ref } from 'vue'
+import { series } from '../assets/series.json'
+
+const seriesList = series;
+const selectedSeriesList = ref([]);
+const count = computed(() => { return selectedSeriesList.value.length; })
+</script>
+
+<template>
+  <h1>Variety List</h1>
+  <h2>{{ count }} / {{ seriesList.length }}</h2>
+
+  <template v-for="series in seriesList" :key="series.name">
+    <div>
+      <input
+        type="checkbox"
+        :id="series.name"
+        :name="series.name"
+        :value="series"
+        v-model="selectedSeriesList"
+      />
+      <label :for="series.name">{{ series.name }}</label>
+    </div>
+  </template>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
